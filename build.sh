49ef4ea8a272901aa20609a2de3ce4624432829e
Xin Zhang <ahjdzx1990@gmail.com>
2023-05-31 18:45:16 +0800

    构建增加etcd

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64 -t harbor.tuhutest.cn/mlp-algo-serving/g6vp:v0.0.2 -f packages/gi-site/docker/gi-site.Dockerfile --network=host .
+docker buildx build --platform linux/amd64 -t harbor.tuhutest.cn/mlp-algo-serving/g6vp:v0.0.3 -f packages/gi-site/docker/gi-site.Dockerfile --network=host .
